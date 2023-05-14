631e716911acaca476920a39b93680eb37640a18
himaloyash <himaloyashis98@gmail.com>
2023-05-14 15:21:31 +0600

    delete added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
 const port = process.env.PORT || 5000
 
@@ -32,6 +32,52 @@ async function run() {
         await client.connect();
 
         const carDoctorCollection = client.db('carDoctorDB').collection('doctor')
+        const carBookingCollection = client.db('carDoctorDB').collection('carDoctor')
+
+        app.get('/services', async(req, res)=>{
+            const cursor = carDoctorCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+
+        })
+
+        app.get('/services/:id', async (req, res)=>{
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const option = {
+                projection:{title:1, img:1 , price:1, service_id:1}
+            }
+            const result = await carDoctorCollection.findOne(query, option)
+            res.send(result)
+        })
+
+        // car booking
+
+        app.get('/bookings', async(req,res)=>{
+            let query = {}
+            if(req.query?.email){
+                query={email: req.query.email}
+            }
+            const result = await carBookingCollection.find(query).toArray()
+            res.send(result)
+            
+        })
+
+        app.post('/bookings', async(req,res)=>{
+            const booking = req.body
+            console.log(booking)
+            const result = await carBookingCollection.insertOne(booking)
+            res.send(result)
+        })
+
+        app.delete('/bookings/:id', async(req, res)=>{
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const result = await carBookingCollection.deleteOne(query)
+            res.send(result)
+            
+        })
+
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
